14c82353e0b400409bc23f064f2a7ba4a666a574
Domengo <dominicsengo@gmail.com>
2024-07-22 19:17:41 +0300

    chore: Update global.css imports and add module-resolver plugin

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -10,6 +10,9 @@
 // }
 import Slot from "expo-router";
 
+import "'@/global.css'";
+import { GluestackUIProvider } from "@/'components/ui'/gluestack-ui-provider";
+
 // Import your global CSS file
 import "../global.css"
 
