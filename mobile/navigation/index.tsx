710fb3be0679c37b9fe688cb056ffe79dc3e6f64
Domengo <dominicsengo@gmail.com>
2024-08-09 01:52:25 +0300

    chore: Update database server and enable password prompt

--- mobile/navigation/index.tsx
+++ mobile/navigation/index.tsx
@@ -35,8 +35,6 @@ const MainTab = () => (
         } else if (route.name === "Settings") {
           iconName = focused ? "settings" : "settings-outline";
         }
-
-        // You can return any component that you like here!
         return <Ionicons name={iconName} size={size} color={color} />;
       },
       tabBarActiveTintColor: "tomato",
